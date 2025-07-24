bf24438aabb3a58234bb4b22558a5814503dffd7
Francesco Visconti Prasca <f.viscontiprasca@gmail.com>
2025-07-24 23:42:59 +0200

    Update 1b_image_split.py

--- 1b_image_split.py
+++ 1b_image_split.py
@@ -1,13 +1,13 @@
 import random, shutil
 from pathlib import Path
 
-raw_dir = Path("all_images/")
+raw_dir = Path("Datasets/gtFinePanopticParts_trainval/all_images")
 manual_dir = Path("Datasets/gtFinePanopticParts_trainval/manual_labeling")
 auto_dir = Path("Datasets/gtFinePanopticParts_trainval/auto_labeling")
 manual_dir.mkdir(parents=True, exist_ok=True)
 auto_dir.mkdir(parents=True, exist_ok=True)
 
-images = list(raw_dir.glob("*.jpg"))
+images = list(raw_dir.glob("*.tif"))
 random.shuffle(images)
 n_manual = int(0.1 * len(images))  # 10% for manual labeling
 
